9bd283b8a9a54bb683a1702632b64f9f140d0a91
David Dieruf <david@dierufventures.com>
2022-12-14 10:04:26 -0500

    Added curl create topic

---
+++ curl/create-topic.sh
@@ -0,0 +1,5 @@
+# Create a new topic in an existing pulsar tenant
+curl -sS --fail --location \
+    --request PUT \
+    --header "Authorization: Bearer ${PULSAR_TOKEN}" \
+    "${WEB_SERVICE_URL}/admin/v3/topics/${TENANT}/${NAMESPACE}/${TOPIC}"
