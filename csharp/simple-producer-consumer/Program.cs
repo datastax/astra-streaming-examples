a594b9f6d5ec215a7adb7c38d971d698cef0d1ea
David Dieruf <david@dierufventures.com>
2022-12-14 14:41:10 -0500

    Making csharp work

--- csharp/simple-producer-consumer/Program.cs
+++ csharp/simple-producer-consumer/Program.cs
@@ -2,26 +2,38 @@
 using DotPulsar;
 using DotPulsar.Extensions;
 
-const string myTopic = "persistent://public/default/mytopic";
+var serviceUrl = "<REPLACE_WITH_SERVICE_URL>";
+var pulsarToken = "<REPLACE_WITH_PULSAR_TOKEN>";
+
+var tenantName = "<REPLACE_WITH_TENANT_NAME>";
+var namespace = "<REPLACE_WITH_NAMESPACE>";
+var topicName = "<REPLACE_WITH_TOPIC>";
+
+var topic = $"persistent:{tenantName}/{namespace}/{topicName}";
 
 await using var client = PulsarClient.Builder()
-                                     .Build();
+                                      .serviceUrl(serviceUrl)
+                                      .authentication(
+                                        AuthenticationFactory.token(pulsarToken)
+                                      )
+                                      .Build();
 // end::build-client[]
 
 // tag::build-producer[]
 await using var producer = client.NewProducer(Schema.String)
-                                 .Topic(myTopic)
+                                 .Topic(topic)
                                  .Create();
 // end::build-producer[]
 
 // tag::produce-message[]
 _ = await producer.Send("Hello World"); // Send a message and ignore the returned MessageId
+Console.WriteLine("Sent message");
 // end::produce-message[]
 
 // tag::build-consumer[]
 await using var consumer = client.NewConsumer(Schema.String)
-                                 .SubscriptionName("MySubscription")
-                                 .Topic(myTopic)
+                                 .SubscriptionName("examples-subscription")
+                                 .Topic(topic)
                                  .InitialPosition(SubscriptionInitialPosition.Earliest)
                                  .Create();
 // end::build-consumer[]
