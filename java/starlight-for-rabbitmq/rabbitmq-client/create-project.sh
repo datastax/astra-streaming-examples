fed7c764887361ec5567fc383c839579470bef60
David Dieruf <david@dierufventures.com>
2023-01-19 16:24:16 -0500

    Created S4R java client example

---
+++ java/starlight-for-rabbitmq/rabbitmq-client/create-project.sh
@@ -0,0 +1,7 @@
+mvn archetype:generate \
+    -DgroupId=org.example \
+    -DartifactId=StarlightForRabbitMqClient \
+    -DarchetypeArtifactId=maven-archetype-quickstart \
+    -DinteractiveMode=false
+
+cd StarlightForRabbitMqClient
